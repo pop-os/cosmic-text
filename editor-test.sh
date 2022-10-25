efc5108a2db5c1429abe0b9e44719916206f6fa1
Jeremy Soller <jackpot51@gmail.com>
2022-10-25 10:18:11 -0600

    Do not use mono feature for editor test

--- editor-test.sh
+++ editor-test.sh
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
 
-RUST_LOG="cosmic_text=debug,editor_test=debug" cargo run --release --package editor-test --features mono -- "$@"
+RUST_LOG="cosmic_text=debug,editor_test=debug" cargo run --release --package editor-test -- "$@"
