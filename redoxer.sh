dbe223acba4652c30a792c32a4d79c1b45c5b81d
Jeremy Soller <jackpot51@gmail.com>
2022-10-27 12:07:05 -0600

    Add redoxer example

---
+++ redoxer.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -ex
+
+rm -rf target/redoxer
+mkdir -p target/redoxer
+
+redoxer install \
+    --no-track \
+    --path examples/editor-orbclient \
+    --root "target/redoxer"
+
+cmd="env RUST_LOG=cosmic_text=debug,editor_orbclient=debug ./bin/editor-orbclient"
+if [ -f "$1" ]
+then
+    filename="$(basename "$1")"
+    cp "$1" "target/redoxer/${filename}"
+    cmd="${cmd} '${filename}'"
+fi
+
+cd target/redoxer
+
+# TODO: remove need for linking fonts
+redoxer exec \
+    --gui \
+    --folder . \
+    /bin/sh -c \
+    "ln -s /ui/fonts /usr/share/fonts && ${cmd}"
