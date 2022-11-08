268805ba0cc16d22321d129b96b23678963be513
Jeremy Soller <jackpot51@gmail.com>
2022-11-08 08:43:27 -0700

    no_std support

--- test.sh
+++ test.sh
@@ -4,5 +4,8 @@ set -ex
 
 cargo doc
 cargo test
+cargo build --release --no-default-features
+cargo build --release --no-default-features --features std
+cargo build --release --no-default-features --features swash
 cargo build --release --all
 env RUST_LOG=editor_test=info target/release/editor-test
