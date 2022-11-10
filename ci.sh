26bc1bd64ce1850c0e5150a4b1658f92dc800d76
Jeremy Soller <jackpot51@gmail.com>
2022-11-10 08:04:53 -0700

    Add prints describing build steps to ci.sh

--- ci.sh
+++ ci.sh
@@ -2,10 +2,23 @@
 
 set -ex
 
+echo Build with default features
 cargo build --release
+
+echo Build with no default features
 cargo build --release --no-default-features
+
+echo Build with only std feature
 cargo build --release --no-default-features --features std
+
+echo Build with only swash feature
 cargo build --release --no-default-features --features swash
+
+echo Build with only syntect feature
 cargo build --release --no-default-features --features syntect
+
+echo Build with all features
 cargo build --release --all-features
-cargo build --release --all
+
+echo Run tests
+cargo test
