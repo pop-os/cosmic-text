3905312f5f727e95505e16e5b49d2da42547f745
grovesNL <josh@joshgroves.com>
2023-08-09 11:42:12 -0230

    Add `no_std` to CI and add `std` per feature

--- ci.sh
+++ ci.sh
@@ -13,20 +13,20 @@ cargo fmt --check
 echo Build with default features
 build
 
-echo Build with no default features
-build --no-default-features
+echo Build with only no_std feature
+build --no-default-features --features no_std
 
 echo Build with only std feature
 build --no-default-features --features std
 
-echo Build with only swash feature
-build --no-default-features --features swash
+echo Build with only std and swash features
+build --no-default-features --features std,swash
 
-echo Build with only syntect feature
-build --no-default-features --features syntect
+echo Build with only std and syntect features
+build --no-default-features --features std,syntect
 
-echo Build with only vi feature
-build --no-default-features --features vi
+echo Build with only std and vi features
+build --no-default-features --features std,vi
 
 echo Build with all features
 build --all-features
