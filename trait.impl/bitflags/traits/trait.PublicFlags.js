eb564258106b5e50bbb009413a88a08a6840749a
jackpot51 <jackpot51@users.noreply.github.com>
2024-03-25 14:56:39 +0000

    deploy: ff5501d9a36e51c50d908413caf7632d8f7533b7

--- trait.impl/bitflags/traits/trait.PublicFlags.js
+++ trait.impl/bitflags/traits/trait.PublicFlags.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "cosmic_text":[["impl PublicFlags for <a class=\"struct\" href=\"cosmic_text/struct.CacheKeyFlags.html\" title=\"struct cosmic_text::CacheKeyFlags\">CacheKeyFlags</a>"]],
-"rustybuzz":[["impl PublicFlags for <a class=\"struct\" href=\"rustybuzz/struct.BufferFlags.html\" title=\"struct rustybuzz::BufferFlags\">BufferFlags</a>"],["impl PublicFlags for <a class=\"struct\" href=\"rustybuzz/struct.SerializeFlags.html\" title=\"struct rustybuzz::SerializeFlags\">SerializeFlags</a>"]]
+"rustybuzz":[["impl PublicFlags for <a class=\"struct\" href=\"rustybuzz/struct.SerializeFlags.html\" title=\"struct rustybuzz::SerializeFlags\">SerializeFlags</a>"],["impl PublicFlags for <a class=\"struct\" href=\"rustybuzz/struct.BufferFlags.html\" title=\"struct rustybuzz::BufferFlags\">BufferFlags</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
