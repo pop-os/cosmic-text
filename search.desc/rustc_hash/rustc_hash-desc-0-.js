671b9829da57788dec8d31e23650be4afd7f4256
jackpot51 <jackpot51@users.noreply.github.com>
2025-08-04 19:50:22 +0000

    deploy: de355a1fd9855e78273d4b7f2b5716eaaa38484f

---
+++ search.desc/rustc_hash/rustc_hash-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("rustc_hash", 0, "Fast, non-cryptographic hash used by rustc and Firefox.\nA speedy hash algorithm for use within rustc. The hashmap …\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.")
---
+++ search.desc/sys_locale/sys_locale-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("sys_locale", 0, "A library to safely and easily obtain the current locale …\nReturns the most preferred locale for the system or …\nReturns the preferred locales for the system or …")
