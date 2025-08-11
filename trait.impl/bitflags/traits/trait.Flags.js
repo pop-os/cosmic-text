636c80961714b67d1e5d9c4b3845e7e4a4b61dd0
jackpot51 <jackpot51@users.noreply.github.com>
2025-08-11 20:17:37 +0000

    deploy: f7033bb0433f6a9ba109007027781ba46ea9ba27

---
+++ trait.impl/bitflags/traits/trait.Flags.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["cosmic_text",[["impl <a class=\"trait\" href=\"bitflags/traits/trait.Flags.html\" title=\"trait bitflags::traits::Flags\">Flags</a> for <a class=\"struct\" href=\"cosmic_text/struct.CacheKeyFlags.html\" title=\"struct cosmic_text::CacheKeyFlags\">CacheKeyFlags</a>"]]],["rustybuzz",[["impl <a class=\"trait\" href=\"bitflags/traits/trait.Flags.html\" title=\"trait bitflags::traits::Flags\">Flags</a> for <a class=\"struct\" href=\"rustybuzz/struct.BufferFlags.html\" title=\"struct rustybuzz::BufferFlags\">BufferFlags</a>"],["impl <a class=\"trait\" href=\"bitflags/traits/trait.Flags.html\" title=\"trait bitflags::traits::Flags\">Flags</a> for <a class=\"struct\" href=\"rustybuzz/struct.SerializeFlags.html\" title=\"struct rustybuzz::SerializeFlags\">SerializeFlags</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[270,511]}
