35817d29c783ef982d86509e0df325ad021c072e
Hojjat <hojjat.abdollahi@gmail.com>
2026-02-24 15:59:37 -0700

    test: text decoration for bidi text is different
    test: bidi and rtl tests for text decoration

--- tests/text_decorations.rs
+++ tests/text_decorations.rs
@@ -49,3 +49,79 @@ fn test_text_decorations() {
         .canvas(600, 50)
         .validate_text_rendering();
 }
+
+
+#[test]
+fn test_text_decorations_rtl() {
+    let base = base_attrs();
+    let red = Color::rgb(0xFF, 0x00, 0x00);
+    let cyan = Color::rgb(0x00, 0xFF, 0xFF);
+
+    DrawTestCfg::new("text_decoration_rtl")
+        .font_size(20., 26.)
+        .font_attrs(base.clone())
+        .rich_text(vec![
+            ("زیر خط ", base.clone().underline(UnderlineStyle::Single)),
+            ("دوتایی ", base.clone().underline(UnderlineStyle::Double)),
+            ("خط ", base.clone().strikethrough()),
+            ("رو ", base.clone().overline()),
+            (
+                "زیر خط قرمز ",
+                base.clone()
+                    .underline(UnderlineStyle::Single)
+                    .underline_color(red),
+            ),
+            (
+                "فیروزه ای ",
+                base.clone().strikethrough().strikethrough_color(cyan),
+            ),
+            (
+                "همگی",
+                base.clone()
+                    .underline(UnderlineStyle::Single)
+                    .strikethrough()
+                    .overline(),
+            ),
+            (" هیچ", base),
+        ])
+        .canvas(600, 50)
+        .validate_text_rendering();
+}
+
+/// Bidi test
+#[test]
+fn test_text_decorations_bidi() {
+    let base = base_attrs();
+    let red = Color::rgb(0xFF, 0x00, 0x00);
+    let cyan = Color::rgb(0x00, 0xFF, 0xFF);
+
+    DrawTestCfg::new("text_decoration_bidi")
+        .font_size(20., 26.)
+        .font_attrs(base.clone())
+        .rich_text(vec![
+            ("زیرخط ", base.clone().underline(UnderlineStyle::Single)),
+            ("Double ", base.clone().underline(UnderlineStyle::Double)),
+            ("خط ", base.clone().strikethrough()),
+            ("Over ", base.clone().overline()),
+            (
+                "Red زیر خط ",
+                base.clone()
+                    .underline(UnderlineStyle::Single)
+                    .underline_color(red),
+            ),
+            (
+                "CyanSt ",
+                base.clone().strikethrough().strikethrough_color(cyan),
+            ),
+            (
+                "All",
+                base.clone()
+                    .underline(UnderlineStyle::Single)
+                    .strikethrough()
+                    .overline(),
+            ),
+            (" Plain", base),
+        ])
+        .canvas(600, 50)
+        .validate_text_rendering();
+}
